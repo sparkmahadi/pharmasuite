5775a31ee30835170063afb20430841ef7db91fc
sparkmahadi <mahadi.development@gmail.com>
2024-08-19 11:22:14 +0600

    integrated controllers on medicines route

--- routes/v1/medicines.route.js
+++ routes/v1/medicines.route.js
@@ -5,5 +5,11 @@ const router = express.Router();
 
 router.route("/")
 .get(medicinesController.getMedicines)
+.post(medicinesController.postMedicine)
+
+router.route("/top").get(medicinesController.getTopMedicines)
+router.route("/latestSold").get(medicinesController.getLatestSoldMedicines)
+
+router.route("/search/:term").get(medicinesController.searchMedicines)
 
 module.exports = router;
